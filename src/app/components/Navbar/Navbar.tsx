d6f674415ad6b569a9b97f51f2ccd215bf88d6b0
Artuross22 <bonjjjuaro@gmail.com>
2024-04-11 21:44:23 +0300

    Added basic layout

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from 'react'
+
+type Props = {
+  }
+  
+  function Navbar({}: Props) {
+    return (
+      <header style={{ backgroundColor: '#282c34', padding: '10px', color: 'white', display: 'flex', justifyContent: 'space-between'}}>
+      <nav>
+        <ul style={{ listStyle: 'none', display: 'flex', gap: '10px' }}>
+          <li><Link href="/" passHref><span style={{ color: 'white', textDecoration: 'none' }}>Home</span></Link></li>
+          <li><Link href="/about" passHref><span style={{ color: 'white', textDecoration: 'none' }}>About</span></Link></li>
+          <li><Link href="/services" passHref><span style={{ color: 'white', textDecoration: 'none' }}>Services</span></Link></li>
+        </ul>
+      </nav>
+      <nav>
+        <ul style={{ listStyle: 'none', display: 'flex', gap: '10px' }}>
+          <li><Link href="/blog" passHref><span style={{ color: 'white', textDecoration: 'none' }}>Investing</span></Link></li>
+          <li><Link href="/contact" passHref><span style={{ color: 'white', textDecoration: 'none' }}>Treading</span></Link></li>
+          <li><Link href="/login" passHref><span style={{ color: 'white', textDecoration: 'none' }}>Algorithms</span></Link></li>
+        </ul>
+      </nav>
+    </header>
+    );
+  };
+
+export default Navbar
