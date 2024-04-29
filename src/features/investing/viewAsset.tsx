a5d1b8851b439b31d4c8714a40cad1f7e15d4ad9
Artuross22 <bonjjjuaro@gmail.com>
2024-04-29 23:19:56 +0300

    Improved asset information page

--- src/features/investing/viewAsset.tsx
+++ src/features/investing/viewAsset.tsx
@@ -1,19 +1,36 @@
 import { getAsset } from '@/utils/asset';
-import React from 'react'
 
-type ViewAssetProps = {
+interface Asset {
   id: string;
+  name: string;
+  currentPrice: number;
+  addedAt: Date;
+  boughtFor: number;
+  profit: number;
+  active: boolean;
+  closedAt: Date;
 }
 
-const ViewAsset: React.FC<ViewAssetProps> = async ({ id }) => { 
-  const asset = await getAsset(id)
+const ViewAsset: React.FC<Asset> = async ({ id }) => {
+  const asset = await getAsset(id);
+
+  if (!asset) {
+    return null; // or handle the case when props is null
+  }
 
   return (
-    <div>    
-      <p>View Asset!</p>
-      <p>{String(asset?.addedAt)}</p>
+    <div style={{ width: '50%', float: 'left' }}>
+      <div style={{ backgroundColor: 'lightgray', margin: '1%', padding: '2%' }}>
+        <h1>{asset.name}</h1>
+        <p>Current Price: {asset.currentPrice}</p>
+        <p>Added At: {asset.addedAt.toLocaleString()}</p>
+        <p>Bought For: {asset.boughtFor}</p>
+        <p>Profit: {asset.currentPrice - asset.boughtFor}</p>
+        <p>Active: {asset.active ? 'Yes' : 'No'}</p>
+        <p>Closed At: {asset.closedAt.toLocaleString()}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ViewAsset
+export default ViewAsset;
